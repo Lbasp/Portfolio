ba1c70f540e3d00334a89952e731411979dd3b49
Lbasper <lbasper.c@gmail.com>
2023-04-02 21:33:02 -0500

    Templates created

--- base/views.py
+++ base/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+project = [
+    {'id':1, 'name': 'probemos1'},
+    {'id':2, 'name': 'miremos'},
+]
+
 
 def home(request):
-    return HttpResponse('Home page')
-
-
+    return render(request, 'base/home.html', {'project':project})
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'base/about.html')
+
 def projects(request):
-    return HttpResponse('Projects')
+    return render(request, 'base/projects.html')
 
 def contact(request):
-    return HttpResponse('Contact')
+    return render(request, 'base/contact.html')
